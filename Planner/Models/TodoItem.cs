fc52b4c12870be141e9c1ddaff7f735b7b93189a
kamclark <kamnmclark@gmail.com>
2019-02-20 19:01:21 -0500

    Created API, Add db context
    added routing for CRUD capabilities

---
+++ Planner/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Priority { get; set; }
+        public string Memo { get; set; }
+        public bool IsComplete { get; set; }
+        public string Icon { get; set; }
+    }
+}
